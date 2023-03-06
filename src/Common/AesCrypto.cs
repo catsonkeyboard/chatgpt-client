ee98ac96b57306cf810be7712c3004e0c3b69b93
m!ng <leemeany@gmail.com>
2023-03-06 23:19:02 +0800

    feat:add apikey encrypt

---
+++ src/Common/AesCrypto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWpfUI.Common
+{
+    public class AesCrypto
+    {
+        private static byte[] IV = new byte[] { /* 初始化向量 */ };
+
+        public static string Encrypt(string plaintext, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 128; // 设置密钥长度
+                aes.Key = keyBytes;
+                aes.IV = IV;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(plaintextBytes, 0, plaintextBytes.Length);
+                    }
+
+                    byte[] ciphertextBytes = ms.ToArray();
+                    return Convert.ToBase64String(ciphertextBytes);
+                }
+            }
+        }
+
+        public static string Decrypt(string ciphertext, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] ciphertextBytes = Convert.FromBase64String(ciphertext);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 128; // 设置密钥长度
+                aes.Key = keyBytes;
+                aes.IV = IV;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(ciphertextBytes, 0, ciphertextBytes.Length);
+                    }
+
+                    byte[] plaintextBytes = ms.ToArray();
+                    return Encoding.UTF8.GetString(plaintextBytes);
+                }
+            }
+        }
+    }
+}
---
+++ src/Common/ComputerInfoUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace ChatWpfUI.Common
+{
+    public class ComputerInfoUtils
+    {
+        public static string GetMacAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            string macAddress = string.Empty;
+
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                    adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddress = adapter.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+
+            return macAddress;
+        }
+    }
+}
